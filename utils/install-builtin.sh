d3d3790af292bc6faf3fdf6fc0e4ff79ef53e097
Johnny Wu <KarasAya@gmail.com>
2015-05-30 12:20:32 +0800

    redirect packages to fireball-packages

--- utils/install-builtin.sh
+++ utils/install-builtin.sh
@@ -8,28 +8,28 @@ cd builtin
 
 # builtin panels
 if [ ! -d "console" ]; then
-    git clone https://github.com/fireball-x/console
+    git clone https://github.com/fireball-packages/console
 fi
 
 if [ ! -d "ipc-debugger" ]; then
-    git clone https://github.com/fireball-x/ipc-debugger
+    git clone https://github.com/fireball-packages/ipc-debugger
 fi
 
 if [ ! -d "package-manager" ]; then
-    git clone https://github.com/fireball-x/package-manager
+    git clone https://github.com/fireball-packages/package-manager
 fi
 
 if [ ! -d "tester" ]; then
-    git clone https://github.com/fireball-x/tester
+    git clone https://github.com/fireball-packages/tester
 fi
 
 # builtin widgets
 if [ ! -d "ui-kit" ]; then
-    git clone https://github.com/fireball-x/ui-kit
+    git clone https://github.com/fireball-packages/ui-kit
 fi
 
 if [ ! -d "pixi-grid" ]; then
-    git clone https://github.com/fireball-x/pixi-grid
+    git clone https://github.com/fireball-packages/pixi-grid
 fi
 
 cd ${ORIGINAL_PATH}
